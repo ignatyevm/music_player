f8f9bda559c2cbff9496b371afd5bdb34ff3409d
polyndrom <polyndromdev@gmail.com>
2020-04-18 17:26:48 +1000

    Basic

---
+++ src/main/java/MusicPlayerMain.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MusicPlayerMain extends Application {
+
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
