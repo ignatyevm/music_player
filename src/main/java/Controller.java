297799b4afe0afa9002b6d5403ee53b702a812c3
polyndrom <polyndromdev@gmail.com>
2020-04-18 21:06:51 +1000

    Controller - event handler for ui components

---
+++ src/main/java/Controller.java
@@ -0,0 +1,13 @@
+import javafx.stage.Stage;
+
+public class Controller {
+
+    private Stage stage;
+    private UIContext uiContext;
+
+    public Controller(Stage stage, UIContext uiContext) {
+        this.stage = stage;
+        this.uiContext = uiContext;
+    }
+
+}
