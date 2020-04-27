0cfc6eaec4e11137f0de1ca34c694ba41af98f9e
polyndrom <polyndromdev@gmail.com>
2020-04-28 04:09:08 +1000

    Refactoring + debug mode

--- src/main/java/MusicPlayerMain.java
+++ src/main/java/MusicPlayerMain.java
@@ -1,18 +1,36 @@
 import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableView;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.Arrays;
 
 public class MusicPlayerMain extends Application {
 
-    public static final int WINDOW_HEIGHT = 500;
-    public static final int WINDOW_WIDTH = 400;
+    public static final int WINDOW_HEIGHT = 400;
+    public static final int WINDOW_WIDTH = 500;
 
-    public static Stage stage;
+    private static Stage stage;
 
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         MusicPlayerMain.stage = stage;
         stage.setTitle("Music Player");
         stage.centerOnScreen();
-        InitialScene.show(stage);
+        boolean debug = true;
+        if (debug) {
+            File testDir = new File(System.getProperty("user.home") + "/_mp_test/");
+            File[] files = testDir.listFiles(file -> file.getName().endsWith(".wav"));
+            MainScene.show(Arrays.asList(files));
+        } else {
+            InitialScene.show();
+        }
+    }
+
+    public static Stage getStage() {
+        return stage;
     }
 
     public static void main(String[] args) {
