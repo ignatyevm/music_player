537fa1a6562b72fa2af4f47f198fe2b1d546d2c3
polyndrom <polyndromdev@gmail.com>
2020-04-28 04:12:39 +1000

    Refactoring

--- src/main/java/InitialScene.java
+++ src/main/java/InitialScene.java
@@ -1,3 +1,4 @@
+import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -7,18 +8,24 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 public class InitialScene extends Scene {
 
-    private InitialScene(Stage stage, Group root) {
+    private InitialScene(Group root) {
         super(root, MusicPlayerMain.WINDOW_WIDTH, MusicPlayerMain.WINDOW_HEIGHT);
 
         Button openFilesButton = new Button("Open file(-s)");
         Button openFolderButton = new Button("Open folder");
 
-        openFilesButton.setOnAction(Controller::onOpenFiles);
-        openFolderButton.setOnAction(Controller::onOpenFolder);
+        openFilesButton.setOnAction(this::onOpenFiles);
+        openFolderButton.setOnAction(this::onOpenFolder);
 
         HBox buttonsBox = new HBox(openFilesButton, new Label("or"), openFolderButton);
         buttonsBox.setAlignment(Pos.CENTER);
@@ -38,10 +45,34 @@ public class InitialScene extends Scene {
         root.getChildren().addAll(borderPane);
     }
 
-    static void show(Stage stage) {
+    private void onOpenFiles(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select file(-s)");
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Audio files", "*.wav"));
+        List<File> selectedFiles = fileChooser.showOpenMultipleDialog(MusicPlayerMain.getStage());
+        if (selectedFiles != null) {
+            MainScene.show(selectedFiles);
+        }
+    }
+
+    private void onOpenFolder(ActionEvent event) {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select folder");
+        directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        File selectedFolder = directoryChooser.showDialog(MusicPlayerMain.getStage());
+        if (selectedFolder != null) {
+            File[] filteredFiles = selectedFolder.listFiles(file -> file.getName().endsWith(".wav"));
+            if (filteredFiles != null) {
+                MainScene.show(Arrays.asList(filteredFiles));
+            }
+        }
+    }
+
+    static void show() {
         Group root = new Group();
-        stage.setScene(new InitialScene(stage, root));
-        stage.show();
+        MusicPlayerMain.getStage().setScene(new InitialScene(root));
+        MusicPlayerMain.getStage().show();
     }
 
 }
