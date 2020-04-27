84894247b36f456c010d7ef6850d8d64a584a99a
polyndrom <polyndromdev@gmail.com>
2020-04-28 04:10:25 +1000

    Main scene (track table, buttons, sliders)

---
+++ src/main/java/MainScene.java
@@ -0,0 +1,122 @@
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.io.File;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MainScene extends Scene {
+
+    public MainSceneController controller;
+    public Player player;
+
+    public TableView<Track> trackTable;
+    public Button prevButton;
+    public Button pauseButton;
+    public Button nextButton;
+    public Label timeLabel;
+    public Slider trackSlider;
+    public Slider volumeSlider;
+
+    public MainScene(Group root, List<File> files) {
+        super(root, MusicPlayerMain.WINDOW_WIDTH, MusicPlayerMain.WINDOW_HEIGHT);
+
+        controller = new MainSceneController(this);
+
+        ObservableList<Track> tracks = files.stream().map(Track::new)
+                .collect(Collectors.toCollection(FXCollections::observableArrayList));
+
+        player = new Player(this, tracks);
+
+        trackTable = setupTable(tracks);
+
+        prevButton = new Button("prev");
+        pauseButton = new Button("pause");
+        nextButton = new Button("next");
+
+        prevButton.setOnAction(controller::selectPrev);
+        pauseButton.setOnAction(controller::onPause);
+        nextButton.setOnAction(controller::selectNext);
+
+        HBox buttonsBar = new HBox(prevButton, pauseButton, nextButton);
+        buttonsBar.setAlignment(Pos.CENTER);
+        buttonsBar.setSpacing(10);
+
+        trackSlider = setupSlider();
+
+        timeLabel = new Label("00.00");
+
+        HBox hBox = new HBox(trackSlider, timeLabel);
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setSpacing(5);
+        hBox.setPrefWidth(getWidth());
+
+        VBox bottomBox = new VBox(hBox, buttonsBar);
+        bottomBox.setPrefHeight(100);
+        bottomBox.setPrefWidth(getWidth());
+        bottomBox.setAlignment(Pos.CENTER);
+        bottomBox.setSpacing(15);
+
+        VBox mainContainer = new VBox(trackTable, bottomBox);
+        mainContainer.setAlignment(Pos.CENTER);
+
+        root.getChildren().addAll(mainContainer);
+        trackTable.getSelectionModel().select(0);
+    }
+
+    private Slider setupSlider() {
+        Slider trackSlider = new Slider();
+        trackSlider.setPrefWidth(400);
+        trackSlider.setMaxWidth(400);
+        trackSlider.setMinWidth(400);
+        trackSlider.setMin(0.0);
+        trackSlider.setMax(100.0);
+        return trackSlider;
+    }
+
+    private TableView<Track> setupTable(ObservableList<Track> tracks) {
+        TableView<Track> table = new TableView<>(tracks);
+        table.setPrefHeight(getHeight() - 100);
+        table.setPrefWidth(getWidth());
+        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        TableColumn<Track, String> numberColumn = new TableColumn<>("");
+        numberColumn.setCellValueFactory(cellData ->
+                new ReadOnlyStringWrapper(Integer.toString(table.getItems().indexOf(cellData.getValue()) + 1)));
+        numberColumn.setMinWidth(25);
+        numberColumn.setPrefWidth(25);
+        numberColumn.setMaxWidth(200);
+        numberColumn.setStyle("-fx-alignment: center;");
+
+        TableColumn<Track, String> nameColumn = new TableColumn<>("Track");
+        nameColumn.setCellValueFactory(cellData ->
+                new ReadOnlyStringWrapper(cellData.getValue().source.getName()));
+
+        TableColumn<Track, String> durationColumn = new TableColumn<>("Duration");
+        durationColumn.setCellValueFactory(cellData -> {
+            int trackDuration = (int) cellData.getValue().media.getDuration().toSeconds();
+            return new ReadOnlyStringWrapper(String.format("%d.%02d",trackDuration / 60, trackDuration % 60));
+        });
+        durationColumn.setMinWidth(75);
+        durationColumn.setMaxWidth(100);
+        durationColumn.setStyle("-fx-alignment: center;");
+
+        table.getColumns().addAll(numberColumn, nameColumn, durationColumn);
+        table.getSelectionModel().selectedItemProperty().addListener(controller::onTrackSelect);
+        return table;
+    }
+
+    static void show(List<File> files) {
+        Group root = new Group();
+        MusicPlayerMain.getStage().setScene(new MainScene(root, files));
+        MusicPlayerMain.getStage().show();
+    }
+
+}
