e2bf6c4a0ad833b6959db0fa9496533ebab3614b
polyndrom <polyndromdev@gmail.com>
2020-05-01 15:38:02 +1000

    Time slider + time label + some useful methods

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,6 +1,7 @@
 import javafx.collections.ObservableList;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 
 public class Player {
 
@@ -14,6 +15,10 @@ public class Player {
         this.tracks = tracks;
     }
 
+    public void setVolume(double volume) {
+        mediaPlayer.setVolume(volume);
+    }
+
     public void pause() {
         mediaPlayer.pause();
     }
@@ -22,12 +27,29 @@ public class Player {
         mediaPlayer.play();
     }
 
+    public void seek(long millis) {
+        mediaPlayer.seek(Duration.millis(millis));
+    }
+
+    public Duration getTotalDuration() {
+        return mediaPlayer.getTotalDuration();
+    }
+
+    public Duration getCurrentTime() {
+        return mediaPlayer.getCurrentTime();
+    }
+
     public boolean isPaused() {
         return mediaPlayer.getStatus().equals(MediaPlayer.Status.PAUSED);
     }
 
+    public boolean isPlaying() {
+        return mediaPlayer.getStatus().equals(MediaPlayer.Status.PLAYING);
+    }
+
     public void play(int trackIndex) {
         if (mediaPlayer != null) {
+            mainScene.trackSlider.valueProperty().removeListener(mainScene.trackSliderChangeListener);
             mediaPlayer.dispose();
         }
         Logger.logPlayTrack(currentTrackIndex, trackIndex,
@@ -35,6 +57,22 @@ public class Player {
         currentTrackIndex = trackIndex;
         Media media = tracks.get(trackIndex).media;
         mediaPlayer = new MediaPlayer(media);
+
+        mainScene.trackSlider.valueProperty().addListener(mainScene.trackSliderChangeListener);
+
+        mediaPlayer.currentTimeProperty().addListener(l-> {
+            mainScene.trackSlider.valueProperty().removeListener(mainScene.trackSliderChangeListener);
+
+            int currentTime = (int) getCurrentTime().toMillis();
+            int totalTime = (int) getTotalDuration().toMillis();
+            mainScene.trackSlider.setValue(currentTime * 100.0 / totalTime);
+
+            int mins = (int) (getCurrentTime().toSeconds() / 60);
+            int secs = (int) (getCurrentTime().toSeconds() % 60);
+            mainScene.timeLabel.setText(String.format("%d:%02d", mins, secs));
+
+            mainScene.trackSlider.valueProperty().addListener(mainScene.trackSliderChangeListener);
+        });
         mediaPlayer.setOnReady(() -> {
             play();
             mediaPlayer.setOnEndOfMedia(() -> {
